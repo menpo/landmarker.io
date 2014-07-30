ccba21bb52a1bedb9cf6909710d5e7ed220786ee
James Booth <jabooth@gmail.com>
2014-07-30 19:51:17 +0100

    move CSS to src/

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
+var prefix = require('gulp-autoprefixer');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var notify = require("gulp-notify");
 
 var paths = {
-    js: ['js/**/*.js']
+    js: ['js/**/*.js'],
+    css: ['src/css/**/*.css']
 };
 
 // Rebuild the JS bundle + issue a notification when done.
@@ -18,11 +20,21 @@ gulp.task('js', function() {
         .pipe(notify('Landmarker.io: JS rebuilt'));
 });
 
+// Rebuild the CSS autoprefixer output + issue a notification when done.
+gulp.task('css', function() {
+    return gulp.src(paths.css)
+        .pipe(prefix(["last 1 version", "> 1%", "ie 8", "ie 7"],
+            { cascade: true }))
+        .pipe(gulp.dest('./css/'))
+        .pipe(notify('Landmarker.io: CSS rebuilt'));
+});
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
     console.log(paths.js);
     gulp.watch(paths.js, ['js']);
+    gulp.watch(paths.css, ['css']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'js']);
+gulp.task('default', ['watch', 'js', 'css']);
