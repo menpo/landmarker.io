44fb6e9cc3b044b4184fa3d082c173163841b3cc
James Booth <jabooth@gmail.com>
2014-07-30 19:53:41 +0100

    move js to src/

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var source = require('vinyl-source-stream');
 var notify = require("gulp-notify");
 
 var paths = {
-    js: ['js/**/*.js'],
+    js: ['src/js/**/*.js'],
     css: ['src/css/**/*.css']
 };
 
 // Rebuild the JS bundle + issue a notification when done.
 gulp.task('js', function() {
-    return browserify('./js/index.js')
+    return browserify('./src/js/index.js')
         .bundle({ debug: true })
         //Pass desired output filename to vinyl-source-stream
         .pipe(source('bundle.js'))
