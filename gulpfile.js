bb511cd1aff5dcfb19f9f7ae181bbd2cea88a254
James Booth <jabooth@gmail.com>
2014-07-31 16:42:04 +0100

    add app caching

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,26 @@ var source = require('vinyl-source-stream');
 var notify = require("gulp-notify");
 var manifest = require('gulp-manifest');
 var runSequence = require('run-sequence');
-var exorcist = require('exorcist');
+var rev = require('gulp-rev');
+var buffer = require('gulp-buffer');
+var replace = require('gulp-replace');
+var inject = require('gulp-inject');
+var rimraf = require('gulp-rimraf');
+var grimraf = require('gulp-rimraf');
 
-var paths = {
+
+var src = {
     js: ['src/js/**/*.js'],
-    css: ['src/css/**/*.css']
+    css: ['src/css/**/*.css'],
+    index: 'src/index.html'
 };
 
-var build_dir = '.';
+var built = {
+    js: './',
+    css: './css/'
+};
 
-var built = [
+var built_globs = [
     './bundle*js',
     './index.html',
     './css/*.css',
@@ -22,14 +32,24 @@ var built = [
 ];
 
 gulp.task('manifest', function(){
-    return gulp.src(built, { base: build_dir })
+    return gulp.src(built_globs, { base: '.' })
         .pipe(manifest({
             hash: true,
             filename: 'lmio.appcache',
             exclude: 'lmio.appcache'
         }))
-        .pipe(gulp.dest(build_dir))
+        .pipe(gulp.dest('.'))
         .pipe(notify('Landmarker.io: Manifest updated'));
+});
+
+gulp.task('clean-js', function () {
+    return gulp.src('./bundle*.js', {read:false})
+        .pipe(grimraf());
+});
+
+gulp.task('clean-css', function () {
+    return gulp.src('./css/*.css', {read:false})
+        .pipe(grimraf());
 });
 
 // Rebuild the JS bundle + issue a notification when done.
@@ -37,15 +57,17 @@ gulp.task('js', function() {
     return browserify('./src/js/index.js')
         .bundle({ debug: true })
         // Cut out the source map...
-        .pipe(exorcist('bundle.js.map'))
+        // .pipe(exorcist('bundle.js.map'))
         // Pass desired output filename to vinyl-source-stream
         .pipe(source('bundle.js'))
         // Start piping stream to tasks!
+        .pipe(buffer())
+        .pipe(rev())
         .pipe(gulp.dest('.'))
         //
         // Ideally we would now like to minify the bundle, whilst keeping the
         // source mapping correct. I can't get this working for now, so we
-        // just ignore it.
+        // just ignore it. But would be something like...
         //.pipe(streamify(uglify()))
         //.pipe(rename('bundle.min.js'))
         //.pipe(gulp.dest('.'))
@@ -54,19 +76,45 @@ gulp.task('js', function() {
 
 // Rebuild the CSS autoprefixer output + issue a notification when done.
 gulp.task('css', function() {
-    return gulp.src(paths.css)
+    // Pipe ./src/css through autoprefixer and store in ./css
+    return gulp.src(src.css)
         .pipe(prefix(["last 1 version", "> 1%", "ie 8", "ie 7"],
             { cascade: true }))
+        .pipe(rev())
         .pipe(gulp.dest('./css/'))
         .pipe(notify('Landmarker.io: CSS rebuilt'));
 });
 
+gulp.task('html', function() {
+    var target = gulp.src('./src/index.html');
+    var sources = gulp.src(built_globs, {read: false});
+    return target.pipe(inject(sources, {addRootSlash: false}))
+        .pipe(gulp.dest('.'))
+        .pipe(notify('Landmarker.io: HTML rebuilt'));
+
+});
+
+gulp.task('build-js', function() {
+    runSequence('clean-js',
+        'js', 'build-html');
+});
+
+gulp.task('build-css', function() {
+    runSequence('clean-css',
+        'css', 'build-html');
+});
+
+gulp.task('build-html', function() {
+    runSequence('html', 'manifest');
+});
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-    console.log(paths.js);
-    gulp.watch(paths.js, ['js']);
-    gulp.watch(paths.css, ['css']);
+    gulp.watch(src.js, ['build-js']);
+    gulp.watch(src.css, ['build-css']);
+    gulp.watch(src.html, ['build-html']);
+    // whenever any built file changes, invalidate the manifest
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'js', 'css']);
+gulp.task('default', ['watch', 'build-js', 'build-css']);
