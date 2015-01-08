ccd53e38a35c6ab838449da2039707389e9786fa
James Booth <jabooth@gmail.com>
2015-01-08 17:01:18 +0000

    tidy up atomic

--- src/js/app/model/atomic.js
+++ src/js/app/model/atomic.js
@@ -10,11 +10,11 @@ var AtomicOperationTracker = Backbone.Model.extend({
         }
     },
 
-    startAtomicOperation: function () {
+    _startAtomicOperation: function () {
         this.set('ATOMIC_OPERATION', true);
     },
 
-    endAtomicOperation: function () {
+    _endAtomicOperation: function () {
         this.set('ATOMIC_OPERATION', false);
     },
 
@@ -34,33 +34,20 @@ var AtomicOperationTracker = Backbone.Model.extend({
                 // we are the highest level atomic lock. Code inside should be
                 // called with a single atomic lock wrapped around it.
 
-                console.log('Starting new atomic operation');
-                atomicTracker.startAtomicOperation();
+                console.log('Starting atomic operation');
+                atomicTracker._startAtomicOperation();
                 f.apply(this, arguments);
-                console.log('Ending atomic operation (should be immediately followed by render)');
-                atomicTracker.endAtomicOperation();
+                console.log('Ending atomic operation');
+                atomicTracker._endAtomicOperation();
             } else {
                 // we are nested inside some other atomic lock. Just call the
                 // function as normal.
-                console.log('Interior atomic operation');
                 f.apply(this, arguments);
             }
         };
-
-
     }
-
 
 });
 
 var atomicTracker = new AtomicOperationTracker;
 module.exports = atomicTracker;
-
-// Use this for atomic args
-//function a(args){
-//    b.apply(this, arguments);
-//}
-//function b(args){
-//    alert(arguments); //arguments[0] = 1, etc
-//}
-//a(1,2,3);​
