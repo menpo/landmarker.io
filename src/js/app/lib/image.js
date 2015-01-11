d17b3a4c053e49c1ea03530b9c8021d337cadc5b
James Booth <jabooth@gmail.com>
2015-01-11 13:23:51 +0000

    remove reliance on THREE.TextureLoader, no caching

--- src/js/app/lib/image.js
+++ src/js/app/lib/image.js
@@ -1,22 +1,36 @@
 var Promise = require('promise-polyfill');
 var THREE = require('three');
 
-module.exports = function (url) {
 
+window.ImagePromise = function (url) {
     return new Promise(function(resolve, reject) {
-
-        var material = new THREE.MeshBasicMaterial(
-            {
-                map: THREE.ImageUtils.loadTexture(
-                    url, new THREE.UVMapping(),
-                    function () {
-                        // the texture is now loaded, yield the material out
-                        // of the promise
-                        resolve(material);
-                    },
-                    reject
-                )
-            }
-        );
+        var img = new Image;
+        img.crossOrigin = '';
+        img.addEventListener('load', function (event) {
+            resolve(img);
+        });
+        img.addEventListener('error', function (event) {
+            reject(event);
+        });
+        img.src = url;
     });
 };
+
+window.TexturePromise = function (url) {
+    var texture = new THREE.Texture(undefined, new THREE.UVMapping());
+    texture.sourceFile = url;
+
+    return ImagePromise(url).then(function(image) {
+            texture.image = image;
+            texture.needsUpdate = true;
+            return texture;
+    });
+};
+
+window.MaterialPromise = function(url) {
+    return TexturePromise(url).then(function (texture) {
+        return new THREE.MeshBasicMaterial({map: texture});
+    });
+};
+
+module.exports = MaterialPromise;
