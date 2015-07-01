0d532b98f32d2e536d4fb93597997976dbfc3dfe
lirsacc <c.lirsac@gmail.com>
2015-07-01 13:43:31 +0100

    Fix location.replace -> location  =

--- src/js/index.js
+++ src/js/index.js
@@ -224,7 +224,7 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
         $('#v150Prompt').click(function () {
             localStorage.setItem('LMIO#v150', Date().toString());
-            window.location.replace('https://github.com/menpo/landmarker.io/wiki/Introducing-Snap-Mode-(v1.5.0)');
+            window.location = 'https://github.com/menpo/landmarker.io/wiki/Introducing-Snap-Mode-(v1.5.0)';
         })
     }
 
