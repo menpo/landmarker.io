789f6709d2828af77e4cacd3a8f3a010ccb06d93
lirsacc <c.lirsac@gmail.com>
2015-07-14 14:27:38 +0100

    Extract keyboard shortcuts in own module + introduce ES6 module syntax

---
+++ src/js/app/view/keyboard.js
@@ -0,0 +1,99 @@
+'use strict';
+
+import $ from 'jquery';
+
+import Modal from './modal';
+
+export default function KeyboardShortcutsHandler (app, viewport) {
+    this._keypress = function (e) {
+        // Don't fire on input fields
+        if ($(e.target).closest("input")[0]) {
+            return;
+        }
+
+        const key = e.which;
+
+        if ((app.isHelpOverlayOn() || !!Modal.active()) && key !== 63) {
+            return;
+        }
+
+        switch (key) {
+            case 100:  // d = [d]elete selected
+                app.landmarks().deleteSelected();
+                $('#viewportContainer').trigger("groupDeselected");
+                break;
+            case 113:  // q = deselect all
+                app.landmarks().deselectAll();
+                $('#viewportContainer').trigger("groupDeselected");
+                break;
+            case 114:  // r = [r]eset camera
+                // TODO fix for multiple cameras (should be in camera controller)
+                viewport.resetCamera();
+                break;
+            case 116:  // t = toggle [t]exture (mesh mode only)
+                if (app.meshMode()) {
+                    app.asset().textureToggle();
+                }
+                break;
+            case 97:  // a = select [a]ll
+                app.landmarks().selectAll();
+                $('#viewportContainer').trigger("groupSelected");
+                break;
+            case 103:  // g = complete [g]roup selection
+                $('#viewportContainer').trigger("completeGroupSelection");
+                break;
+            case 99:  // c = toggle [c]amera mode
+                if (app.meshMode()) {
+                    viewport.toggleCamera();
+                }
+                break;
+            case 106:  // j = down, next asset
+                app.nextAsset();
+                break;
+            case 107:  // k = up, previous asset
+                app.previousAsset();
+                break;
+            case 108:  // l = toggle [l]inks
+                app.toggleConnectivity();
+                break;
+            case 101:  // e = toggle [e]dit mode
+                app.toggleEditing();
+                break;
+            case 63: // toggle help
+                app.toggleHelpOverlay();
+                break;
+        }
+    };
+
+    this._keydown = function (evt) {
+        if (evt.which !== 27) {
+            return;
+        }
+
+        if (app.isHelpOverlayOn()) {
+            app.toggleHelpOverlay();
+            return;
+        }
+
+        const modal = Modal.active();
+        if (modal) {
+            if (modal.closable) {
+                modal.close();
+            }
+            return;
+        }
+
+        app.landmarks().deselectAll();
+        $('#viewportContainer').trigger("groupDeselected");
+    };
+}
+
+KeyboardShortcutsHandler.prototype.enable = function () {
+    $(window).on('keypress', this._keypress);
+    $(window).on('keydown', this._keydown);
+};
+
+KeyboardShortcutsHandler.prototype.disable = function () {
+    $(window).off('keypress', this._keypress);
+    $(window).off('keydown', this._keydown);
+};
