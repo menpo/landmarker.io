feeac1564eb366578cbcc03f4455cb9830ea33d5
James Booth <jabooth@gmail.com>
2016-05-22 16:40:19 +0100

    clarification around direction with elements BB rm

--- src/js/app/view/viewport/elements.js
+++ src/js/app/view/viewport/elements.js
@@ -13,26 +13,25 @@ const LM_SPHERE_UNSELECTED_COLOR = 0xffff00;
 const LM_CONNECTION_LINE_COLOR = LM_SPHERE_UNSELECTED_COLOR;
 
 // create a single geometry + material that will be shared by all landmarks
-const lmGeometry = new THREE.SphereGeometry(
+const LM_GEOMETRY = new THREE.SphereGeometry(
     LM_SCALE,
     LM_SPHERE_PARTS,
     LM_SPHERE_PARTS);
 
-const lmMaterialForSelected = {
+const LM_MATERIAL_FOR_SELECTED = {
     true: new THREE.MeshBasicMaterial({color: LM_SPHERE_SELECTED_COLOR}),
     false: new THREE.MeshBasicMaterial({color: LM_SPHERE_UNSELECTED_COLOR})
 };
 
-const lineMaterial = new THREE.LineBasicMaterial({
+const LINE_MATERIAL = new THREE.LineBasicMaterial({
     color: LM_CONNECTION_LINE_COLOR,
     linewidth: 1
 });
 
-function createSphere(position, index, selected) {
-    const landmark = new THREE.Mesh(lmGeometry, lmMaterialForSelected[selected]);
+function createSphere(index) {
+    const landmark = new THREE.Mesh(LM_GEOMETRY, LM_MATERIAL_FOR_SELECTED[false]);
     landmark.name = 'Landmark ' + index;
     landmark.userData.index = index;
-    landmark.position.copy(position);
     return landmark
 }
 
@@ -41,7 +40,7 @@ function createLine(start, end) {
     geometry.dynamic = true;
     geometry.vertices.push(start.clone());
     geometry.vertices.push(end.clone());
-    return new THREE.Line(geometry, lineMaterial)
+    return new THREE.Line(geometry, LINE_MATERIAL)
 }
 
 
@@ -55,11 +54,11 @@ export const LandmarkTHREEView = Backbone.View.extend({
         // null if the landmark isEmpty
         this.symbol = null;
 
+        this.render();
+
         // TODO BB to remove
         _.bindAll(this, 'render');
         this.listenTo(this.model, "change", this.render);
-
-        this.render();
     },
 
     render: function () {
@@ -76,20 +75,20 @@ export const LandmarkTHREEView = Backbone.View.extend({
             // there is no symbol yet
             if (!this.model.isEmpty()) {
                 // and there should be! Make it and update it
-                this.symbol = createSphere(this.model.point(), this.model.index(), this.model.isSelected());
+                this.symbol = createSphere(this.model.index());
                 this.updateSymbol();
                 // and add it to the scene
                 this.onCreate(this.symbol);
             }
         }
-        // trigger the update callback
+        // TODO BB to remove (should be handled at Viewport level)
         this.onUpdate();
     },
 
     updateSymbol: function () {
         this.symbol.position.copy(this.model.point());
         var selected = this.model.isSelected();
-        this.symbol.material = lmMaterialForSelected[selected];
+        this.symbol.material = LM_MATERIAL_FOR_SELECTED[selected];
     },
 
     dispose: function () {
@@ -103,16 +102,17 @@ export const LandmarkTHREEView = Backbone.View.extend({
 export const LandmarkConnectionTHREEView = Backbone.View.extend({
 
     initialize: function (options) {
-        // BB to remove
-        this.listenTo(this.model[0], "change", this.render);
-        this.listenTo(this.model[1], "change", this.render);
-
         this.onCreate = options.onCreate;
         this.onDispose = options.onDispose;
         this.onUpdate = options.onUpdate;
         this.symbol = null; // a THREE object that represents this connection.
         // null if the landmark isEmpty
         this.render();
+
+
+        // TODO BB to remove
+        this.listenTo(this.model[0], "change", this.render);
+        this.listenTo(this.model[1], "change", this.render);
     },
 
     render: function () {
@@ -137,7 +137,7 @@ export const LandmarkConnectionTHREEView = Backbone.View.extend({
                 this.onCreate(this.symbol);
             }
         }
-        // trigger the update callback
+        // TODO BB to remove (should be handled at Viewport level)
         this.onUpdate();
     },
 
