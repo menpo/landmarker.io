37103414f04f01b02a84705a9950509b1f2b1841
James Booth <jabooth@gmail.com>
2016-05-24 07:29:47 +0100

    fix sort difference not boolean (TSC caught it...)

--- src/js/app/view/viewport/handler.js
+++ src/js/app/view/viewport/handler.js
@@ -9,7 +9,7 @@ import atomic from '../../model/atomic';
 const findClosestLandmarks = (lms, point, n = 4) =>
     lms
         .map(lm => ({ landmark: lm, distance: point.distanceTo(lm.point) }))
-        .sort((a, b) => a.distance >= b.distance)
+        .sort((a, b) => a.distance - b.distance)
         .slice(0, n)
         .map(lmd => lmd.landmark);
 
