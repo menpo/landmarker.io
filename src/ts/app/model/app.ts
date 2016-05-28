f75601f0bd5fd8230a56b4520d6bdb003de93752
James Booth <jabooth@gmail.com>
2016-05-28 19:26:12 +0100

    move app to class

--- src/ts/app/model/app.ts
+++ src/ts/app/model/app.ts
@@ -1,18 +1,21 @@
-'use strict';
+import * as $ from 'jquery'
+import * as Backbone from 'backbone'
 
-import * as _ from 'underscore';
-import * as $ from 'jquery';
-import * as Backbone from 'backbone';
+import Tracker from '../lib/tracker'
+import * as AssetSource from './assetsource'
+import { LandmarkGroup } from './landmark'
+import Modal from '../view/modal'
 
-import Tracker from '../lib/tracker';
-import * as AssetSource from './assetsource';
-import { LandmarkGroup } from './landmark';
-import Modal from '../view/modal';
+type AppOptions = {
+    _activeTemplate?: string
+    _activeCollection?: string
+    _assetIndex?: number
+}
 
-export default Backbone.Model.extend({
+export default class App extends Backbone.Model {
 
-    defaults: function () {
-        return {
+    constructor(opts: AppOptions) {
+        super({
             landmarkSize: 0.2,
             mode: 'mesh',
             connectivityOn: true,
@@ -22,194 +25,191 @@ export default Backbone.Model.extend({
             activeCollection: undefined,
             helpOverlayIsDisplayed: false,
             tracker: {}
-        };
-    },
+        })
+        this.set(opts)
 
-    isConnectivityOn: function () {
-        return this.get('connectivityOn');
-    },
+        // New collection? Need to find the assets on them again
+        this.listenTo(this, 'change:activeCollection', this.reloadAssetSource)
+        this.listenTo(this, 'change:activeTemplate', this.reloadLandmarks)
 
-    isAutoSaveOn: function () {
-        return this.get('autoSaveOn');
-    },
+        this._initTemplates()
+        this._initCollections()
+    }
 
-    toggleAutoSave: function () {
-        return this.set('autoSaveOn', !this.isAutoSaveOn());
-    },
+    isConnectivityOn() {
+        return this.get('connectivityOn')
+    }
 
-    toggleConnectivity: function () {
-        this.set('connectivityOn', !this.isConnectivityOn());
-    },
+    isAutoSaveOn() {
+        return this.get('autoSaveOn')
+    }
 
-    isEditingOn: function () {
-        return this.get('editingOn');
-    },
+    toggleAutoSave() {
+        return this.set('autoSaveOn', !this.isAutoSaveOn())
+    }
 
-    toggleEditing: function () {
-        this.set('editingOn', !this.isEditingOn());
+    toggleConnectivity() {
+        this.set('connectivityOn', !this.isConnectivityOn())
+     }
+
+    isEditingOn() {
+        return this.get('editingOn')
+     }
+
+    toggleEditing() {
+        this.set('editingOn', !this.isEditingOn())
         if (!this.isEditingOn() && this.landmarks()) {
-            this.landmarks().deselectAll();
-            this.landmarks().resetNextAvailable();
+            this.landmarks().deselectAll()
+            this.landmarks().resetNextAvailable()
         }
-    },
+     }
 
-    isHelpOverlayOn: function () {
-        return this.get('helpOverlayIsDisplayed');
-    },
+    isHelpOverlayOn() {
+        return this.get('helpOverlayIsDisplayed')
+     }
 
-    toggleHelpOverlay: function () {
-        this.set('helpOverlayIsDisplayed', !this.isHelpOverlayOn());
-    },
+    toggleHelpOverlay() {
+        this.set('helpOverlayIsDisplayed', !this.isHelpOverlayOn())
+     }
 
-    mode: function () {
-        return this.get('mode');
-    },
+    mode() {
+        return this.get('mode')
+     }
 
-    imageMode: function () {
-        return this.get('mode') === 'image';
-    },
+    imageMode() {
+        return this.get('mode') === 'image'
+     }
 
-    meshMode: function () {
-        return this.get('mode') === 'mesh';
-    },
+    meshMode() {
+        return this.get('mode') === 'mesh'
+     }
 
-    server: function () {
-        return this.get('server');
-    },
+    server() {
+        return this.get('server')
+     }
 
-    templates: function () {
-        return this.get('templates');
-    },
+    templates() {
+        return this.get('templates')
+     }
 
-    activeTemplate: function () {
-        return this.get('activeTemplate');
-    },
+    activeTemplate() {
+        return this.get('activeTemplate')
+     }
 
-    collections: function () {
-        return this.get('collections');
-    },
+    collections() {
+        return this.get('collections')
+     }
 
-    activeCollection: function () {
-        return this.get('activeCollection');
-    },
+    activeCollection() {
+        return this.get('activeCollection')
+     }
 
-    tracker: function () {
-            return this.get('tracker');
-    },
+    tracker() {
+            return this.get('tracker')
+     }
 
-    hasAssetSource: function () {
-        return this.has('assetSource');
-    },
+    hasAssetSource() {
+        return this.has('assetSource')
+     }
 
-    assetSource: function () {
-        return this.get('assetSource');
-    },
+    assetSource = () => {
+        return this.get('assetSource')
+     }
 
-    assetIndex: function () {
+    assetIndex() {
         if (this.has('assetSource')) {
-            return this.get('assetSource').assetIndex();
+            return this.get('assetSource').assetIndex()
         }
-    },
+     }
 
     // returns the currently active Asset (Image or Asset).
     // changes independently of mesh() - care should be taken as to which one
     // other objects should listen to.
-    asset: function () {
-        return this.get('asset');
-    },
+    asset() {
+        return this.get('asset')
+     }
 
     // returns the currently active THREE.Mesh.
-    mesh: function () {
+    mesh = () => {
         if (this.hasAssetSource()) {
-            return this.assetSource().mesh();
+            return this.assetSource().mesh()
         } else {
-            return null;
+            return null
         }
-    },
+     }
 
-    landmarks: function () {
-        return this.get('landmarks');
-    },
+    landmarks = () => {
+        return this.get('landmarks')
+     }
 
-    landmarkSize: function () {
-        return this.get('landmarkSize');
-    },
+    landmarkSize() {
+        return this.get('landmarkSize')
+     }
 
-    initialize: function () {
-        _.bindAll(this, 'assetChanged', 'mesh', 'assetSource', 'landmarks');
-
-        // New collection? Need to find the assets on them again
-        this.listenTo(this, 'change:activeCollection', this.reloadAssetSource);
-        this.listenTo(this, 'change:activeTemplate', this.reloadLandmarks);
-
-        this._initTemplates();
-        this._initCollections();
-    },
-
-    budgeLandmarks: function(vector:  [number, number]) {
+    budgeLandmarks(vector:  [number, number]) {
         // call our onBudgeLandmarks callback
         this.onBudgeLandmarks(vector)
-    },
+     }
 
-    _initTemplates: function (override=false) {
+    _initTemplates(override=false) {
         // firstly, we need to find out what template we will use.
         // construct a template labels model to go grab the available labels.
         this.server().fetchTemplates().then((templates) => {
-            this.set('templates', templates);
-            let selected = templates[0];
+            this.set('templates', templates)
+            let selected = templates[0]
             if (!override && this.has('_activeTemplate')) {
                 // user has specified a preset! Use that if we can
                 // TODO should validate here if we can actually use template
-                const preset = this.get('_activeTemplate');
+                const preset = this.get('_activeTemplate')
                 if (templates.indexOf(preset) > -1) {
-                    selected = preset;
+                    selected = preset
                 }
             }
-            this.set('activeTemplate', selected);
+            this.set('activeTemplate', selected)
         }, () => {
             throw new Error('Failed to talk server for templates (is ' +
-                            'landmarkerio running from your command line?).');
-        });
-    },
+                            'landmarkerio running from your command line?).')
+        })
+     }
 
-    _initCollections: function (override=false) {
+    _initCollections(override=false) {
         this.server().fetchCollections().then((collections) => {
-            this.set('collections', collections);
-            let selected = collections[0];
+            this.set('collections', collections)
+            let selected = collections[0]
             if (!override && this.has('_activeCollection')) {
-                const preset = this.get('_activeCollection');
+                const preset = this.get('_activeCollection')
                 if (collections.indexOf(preset) > -1) {
-                    selected = preset;
+                    selected = preset
                 }
             }
-            this.set('activeCollection', selected);
+            this.set('activeCollection', selected)
         }, () => {
             throw new Error('Failed to talk server for collections (is ' +
-                            'landmarkerio running from your command line?).');
-        });
-    },
+                            'landmarkerio running from your command line?).')
+        })
+     }
 
-    reloadAssetSource: function () {
+    reloadAssetSource() {
         // needs to have an activeCollection to preceed. AssetSource should be
         // updated every time the active collection is updated.
         if (!this.get('activeCollection')) {
             // can only proceed with an activeCollection...
-            console.log('App:reloadAssetSource with no activeCollection - doing nothing');
-            return;
+            console.log('App:reloadAssetSource with no activeCollection - doing nothing')
+            return
         }
 
-        console.log('App: reloading asset source');
+        console.log('App: reloading asset source')
 
-        let oldIndex;
+        let oldIndex
         if (this.has('asset') && this.has('assetSource')) {
-            const idx = this.assetSource().assetIndex(this.asset());
+            const idx = this.assetSource().assetIndex(this.asset())
             if (idx > -1) {
-                oldIndex = idx;
+                oldIndex = idx
             }
         }
 
         if (this.hasChanged('mode')) {
-            $('#viewportContainer').trigger('resetCamera');
+            $('#viewportContainer').trigger('resetCamera')
         }
 
         // Construct an asset source (which can query for asset information
@@ -229,89 +229,89 @@ export default Backbone.Model.extend({
         this.listenTo(assetSource, 'change:mesh', this.meshChanged)
 
         assetSource.fetch().then(() => {
-            let i;
+            let i
 
-            console.log('assetSource retrieved - setting');
+            console.log('assetSource retrieved - setting')
 
             if (oldIndex >= 0 && !this.hasChanged('activeCollection')) {
-                i = oldIndex;
+                i = oldIndex
             } else if (this.has('_assetIndex') && !this.has('asset')) {
-                i = this.get('_assetIndex');
+                i = this.get('_assetIndex')
             } else {
-                i = 0;
+                i = 0
             }
 
             if (i < 0 || i > assetSource.nAssets() - 1) {
-                throw Error(`Error trying to set index to ${i} - needs to be in the range 0-${assetSource.nAssets()}`);
+                throw Error(`Error trying to set index to ${i} - needs to be in the range 0-${assetSource.nAssets()}`)
             }
 
-            return this.goToAssetIndex(i);
+            return this.goToAssetIndex(i)
         }, () => {
             throw new Error('Failed to fetch assets (is landmarkerio' +
-                            'running from your command line?).');
-        });
-    },
+                            'running from your command line?).')
+        })
+     }
 
-    getTracker: function (assetId, template) {
-        const tracker = this.tracker();
+    getTracker(assetId, template) {
+        const tracker = this.tracker()
         if (!tracker[assetId]) {
-            tracker[assetId] = {};
+            tracker[assetId] = {}
         }
 
         if (!tracker[assetId][template]) {
-            tracker[assetId][template] = new Tracker();
+            tracker[assetId][template] = new Tracker()
         }
 
-        return tracker[assetId][template];
-    },
+        return tracker[assetId][template]
+     }
 
-    reloadLandmarks: function () {
+    reloadLandmarks() {
         if (this.landmarks() && this.asset()) {
             this.autoSaveWrapper(() => {
-                this.set('landmarks', null);
+                this.set('landmarks', null)
                 this.loadLandmarksPromise().then((lms) => {
-                    this.set('landmarks', lms);
-                });
-            });
+                    this.set('landmarks', lms)
+                })
+            })
         }
-    },
+     }
 
-    autoSaveWrapper: function (fn) {
-        const lms = this.landmarks();
+    autoSaveWrapper(fn) {
+        const lms = this.landmarks()
         if (lms && !lms.tracker.isUpToDate()) {
             if (!this.isAutoSaveOn()) {
-                Modal.confirm('You have unsaved changes, are you sure you want to proceed? (Your changes will be lost). Turn autosave on to save your changes by default.', fn);
+                Modal.confirm('You have unsaved changes, are you sure you want to proceed? (Your changes will be lost). Turn autosave on to save your changes by default.', fn)
             } else {
-                lms.save().then(fn);
+                lms.save().then(fn)
             }
         } else {
-            fn();
+            fn()
         }
-    },
+     }
 
-    _assetSourceConstructor: function () {
+    _assetSourceConstructor() {
         if (this.imageMode()) {
-            return AssetSource.ImageSource;
+            return AssetSource.ImageSource
         } else if (this.meshMode()) {
-            return AssetSource.MeshSource;
+            return AssetSource.MeshSource
         } else {
             console.error('WARNING - illegal mode setting on app! Must be' +
-                ' mesh or image');
+                ' mesh or image')
         }
-    },
+     }
 
     // Mirror the state of the asset source onto the app
-    assetChanged: function () {
-        console.log('App.assetChanged');
-        this.set('asset', this.assetSource().asset());
-    },
+    assetChanged = () => {
+        console.log('App.assetChanged')
+        this.set('asset', this.assetSource().asset())
+     }
 
-    meshChanged: function () {
-        console.log('App.meshChanged');
-        this.trigger('newMeshAvailable');
-    },
+    meshChanged() {
+        console.log('App.meshChanged')
+        this.trigger('newMeshAvailable')
+     }
 
-    loadLandmarksPromise: function () {
+    loadLandmarksPromise() {
         return this.server().fetchLandmarkGroup(
             this.asset().id,
             this.activeTemplate()
@@ -322,91 +322,91 @@ export default Backbone.Model.extend({
                 this.activeTemplate(),
                 this.server(),
                 this.getTracker(this.asset().id, this.activeTemplate())
-            );
+            )
         }, () => {
-            console.log('Error in fetching landmark JSON file');
-        });
-    },
+            console.log('Error in fetching landmark JSON file')
+        })
+     }
 
-    _promiseLandmarksWithAsset: function (loadAssetPromise) {
+    _promiseLandmarksWithAsset(loadAssetPromise) {
         // returns a promise that will only resolve when the asset and
         // landmarks are both downloaded and ready.
 
         // if both come true, then set the landmarks
         return Promise.all([this.loadLandmarksPromise(),
                             loadAssetPromise]).then((args) => {
-            const landmarks = args[0];
+            const landmarks = args[0]
             console.log('landmarks are loaded and the asset is at a suitable ' +
-                'state to display');
+                'state to display')
             // now we know that this is resolved we set the landmarks on the
             // app. This way we know the landmarks will always be set with a
             // valid asset.
-            this.set('landmarks', landmarks);
-        });
-    },
+            this.set('landmarks', landmarks)
+        })
+     }
 
-    _switchToAsset: function (newAssetPromise) {
+    _switchToAsset(newAssetPromise) {
         // The asset promise should come from the assetSource and will only
         // resolve when all the key data for annotating is loaded, the
         // promiseLandmark wraps it and only resolves when both landmarks (if
         // applicable) and asset data are present
         if (newAssetPromise) {
-            this.set('landmarks', null);
-            return this._promiseLandmarksWithAsset(newAssetPromise);
+            this.set('landmarks', null)
+            return this._promiseLandmarksWithAsset(newAssetPromise)
         }
-    },
+     }
 
-    nextAsset: function () {
+    nextAsset() {
         if (this.assetSource().hasSuccessor()) {
             this.autoSaveWrapper(() => {
-                this._switchToAsset(this.assetSource().next());
-            });
+                this._switchToAsset(this.assetSource().next())
+            })
         }
-    },
+     }
 
-    previousAsset: function () {
+    previousAsset() {
         if (this.assetSource().hasPredecessor()) {
             this.autoSaveWrapper(() => {
-                this._switchToAsset(this.assetSource().previous());
-            });
+                this._switchToAsset(this.assetSource().previous())
+            })
         }
-    },
+     }
 
-    goToAssetIndex: function (newIndex) {
+    goToAssetIndex(newIndex) {
         this.autoSaveWrapper(() => {
-            this._switchToAsset(this.assetSource().setIndex(newIndex));
-        });
-    },
+            this._switchToAsset(this.assetSource().setIndex(newIndex))
+        })
+     }
 
-    reloadLandmarksFromPrevious: function () {
-        const lms = this.landmarks();
+    reloadLandmarksFromPrevious() {
+        const lms = this.landmarks()
         if (lms) {
-            const as = this.assetSource();
+            const as = this.assetSource()
             if (this.assetSource().hasPredecessor()) {
                 this.server().fetchLandmarkGroup(
                     as.assets()[as.assetIndex() - 1].id,
                     this.activeTemplate()
                 ).then((json) => {
-                    lms.tracker.recordState(lms.toJSON());
-                    lms.restore(json);
-                    lms.tracker.recordState(lms.toJSON(), false, true);
+                    lms.tracker.recordState(lms.toJSON())
+                    lms.restore(json)
+                    lms.tracker.recordState(lms.toJSON(), false, true)
                 }, () => {
-                    console.log('Error in fetching landmark JSON file');
-                });
+                    console.log('Error in fetching landmark JSON file')
+                })
             }
         }
-    },
+     }
 
-    incrementLandmarkSize: function () {
-        const size = this.get('landmarkSize');
-        const factor = Math.floor(size / 0.25) + 1;
-        this.set('landmarkSize', Math.min(0.25 * factor, 1));
-    },
+    incrementLandmarkSize() {
+        const size = this.get('landmarkSize')
+        const factor = Math.floor(size / 0.25) + 1
+        this.set('landmarkSize', Math.min(0.25 * factor, 1))
+     }
 
-    decrementLandmarkSize: function () {
-        const size = this.get('landmarkSize');
-        const factor = Math.floor(size / 0.25) - 1;
-        this.set('landmarkSize', Math.max(0.25 * factor, 0.05));
+    decrementLandmarkSize() {
+        const size = this.get('landmarkSize')
+        const factor = Math.floor(size / 0.25) - 1
+        this.set('landmarkSize', Math.max(0.25 * factor, 0.05))
     }
 
-});
+}
