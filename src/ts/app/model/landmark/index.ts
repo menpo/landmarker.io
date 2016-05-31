643b1c206bee51fbf1c8f75517fb3d3e76feb6f0
James Booth <jabooth@gmail.com>
2016-05-31 19:46:40 +0100

    expose more landmark classes from module

--- src/ts/app/model/landmark/index.ts
+++ src/ts/app/model/landmark/index.ts
@@ -1,2 +1,4 @@
 export { Landmark } from './landmark'
 export { LandmarkGroup, LJSONFile, LandmarkGroupTracker, landmarkGroupTrackerFactory } from './group'
+export { LandmarkCollection } from './collection'
+export { LandmarkLabel } from './label'
