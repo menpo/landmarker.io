e834757c2b729d6bc0174522965f083391896768
Epameinondas Antonakos <antonakosn@gmail.com>
2016-06-06 17:52:36 +0100

    Update ibug68.js

--- src/js/app/template/ibug68.js
+++ src/js/app/template/ibug68.js
@@ -4,14 +4,6 @@ export default {
         'points': 17,
         'connectivity': ['0 1', '1 2', '2 3', '3 4', '4 5', '5 6', '6 7', '7 8', '8 9', '9 10', '10 11', '11 12', '12 13', '13 14', '14 15', '15 16']
     }, {
-        'label': 'leye',
-        'points': 6,
-        'connectivity': ['0 1', '1 2', '2 3', '3 4', '4 5', '5 0']
-    }, {
-        'label': 'reye',
-        'points': 6,
-        'connectivity': ['0 1', '1 2', '2 3', '3 4', '4 5', '5 0']
-    }, {
         'label': 'leyebrow',
         'points': 5,
         'connectivity': ['0 1', '1 2', '2 3', '3 4']
@@ -19,6 +11,14 @@ export default {
         'label': 'reyebrow',
         'points': 5,
         'connectivity': ['0 1', '1 2', '2 3', '3 4']
+    }, {
+        'label': 'leye',
+        'points': 6,
+        'connectivity': ['0 1', '1 2', '2 3', '3 4', '4 5', '5 0']
+    }, {
+        'label': 'reye',
+        'points': 6,
+        'connectivity': ['0 1', '1 2', '2 3', '3 4', '4 5', '5 0']
     }, {
         'label': 'nose',
         'points': 9,
