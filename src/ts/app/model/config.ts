faab0ca49d0ba10662bf275e9992a7f11f5949c5
James Booth <jabooth@gmail.com>
2016-06-24 18:54:35 +0100

    move config to be typed

--- src/ts/app/model/config.ts
+++ src/ts/app/model/config.ts
@@ -9,73 +9,74 @@ import * as support from '../lib/support'
 
 const LOCALSTORAGE_KEY = 'LMIO#CONFIG'
 
-export function Config () {
-    this._data = {}
-}
+class Config {
+    _data = {}
 
-Config.prototype.get = function (key) {
-    if (!key) {
-        return _.clone(this._data)
-    } else {
-        return this._data[key]
-    }
-}
-
-Config.prototype.has = function (key) {
-    return this._data.hasOwnProperty(key)
-}
-
-Config.prototype.delete = function (key, save) {
-    delete this._data[key]
-    if (save) {
-        this.save()
-    }
-}
-
-Config.prototype.set = function (arg1, arg2, arg3) {
-
-    let save
-
-    if (typeof arg1 === 'string') { // Submitted a key/value pair
-        this._data[arg1] = arg2
-        save = !!arg3
-    } else if (typeof arg1 === 'object') { // Submitted a set of pairs
-        Object.keys(arg1).forEach((k) => {
-            this._data[k] = arg1[k]
-        })
-        save = !!arg2
-    }
-
-    if (save) {
-        this.save()
-    }
-}
-
-Config.prototype.save = function () {
-    if (support.localstorage) {
-        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(this._data))
-    }
-}
-
-Config.prototype.load = function () {
-    if (support.localstorage) {
-        var data = localStorage.getItem(LOCALSTORAGE_KEY)
-        if (data) {
-            this._data = JSON.parse(data)
+    get(key:string) {
+        if (!key) {
+            return _.clone(this._data)
         } else {
-            this._data = {}
+            return this._data[key]
         }
     }
-}
 
-Config.prototype.clear = function () {
-    if (support.localstorage) {
-        localStorage.removeItem(LOCALSTORAGE_KEY)
+    has(key: string) {
+        return this._data.hasOwnProperty(key)
     }
-    this._data = {}
+
+    delete(key: string, save: boolean) {
+        delete this._data[key]
+        if (save) {
+            this.save()
+        }
+    }
+
+    set(arg1: string | {}, arg2: {} | boolean, arg3: boolean) {
+
+        let save: boolean
+
+        if (typeof arg1 === 'string') { // Submitted a key/value pair
+            this._data[arg1] = arg2
+            save = !!arg3
+        } else if (typeof arg1 === 'object') { // Submitted a set of pairs
+            Object.keys(arg1).forEach((k) => {
+                this._data[k] = arg1[k]
+            })
+            save = !!arg2
+        }
+
+        if (save) {
+            this.save()
+        }
+    }
+
+    save() {
+        if (support.localstorage) {
+            localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(this._data))
+        }
+    }
+
+    load() {
+        if (support.localstorage) {
+            const data = localStorage.getItem(LOCALSTORAGE_KEY)
+            if (data) {
+                this._data = JSON.parse(data)
+            } else {
+                this._data = {}
+            }
+        }
+    }
+
+    clear() {
+        if (support.localstorage) {
+            localStorage.removeItem(LOCALSTORAGE_KEY)
+        }
+        this._data = {}
+    }
+
 }
 
-let _configInstance
+let _configInstance: Config
 
 export default function () {
     if (!_configInstance) {
