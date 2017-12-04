8869bb64ab2e1c54612332a17c6d89d9864b52cb
makalekseev <mak.alekseev@gmail.com>
2017-12-04 12:21:08 +0200

    change apikey

--- src/js/app/backend/dropbox.js
+++ src/js/app/backend/dropbox.js
@@ -10,7 +10,7 @@
 'use strict';
 // const API_KEY = 'lar7e1dae96efyx',
 
-const API_KEY = 'd8ipadlxeezrpkz',
+const API_KEY = 'lar7e1dae96efyx',
     API_URL = 'https://api.dropboxapi.com/2',
     CONTENTS_URL = 'https://content.dropboxapi.com/2';
 
